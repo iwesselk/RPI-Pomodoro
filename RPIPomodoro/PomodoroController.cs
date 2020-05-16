6d8cf4ad0c55093fb1fadb211c8474d544e1125f
Ian W <35615053+iwesselk@users.noreply.github.com>
2020-05-15 18:09:07 -0600

    Add project files.

---
+++ RPIPomodoro/PomodoroController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Data;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace RPIPomodoro
+{
+    class PomodoroController
+    {
+        const int SleepTime = 250;
+        private static DateTime? FutureDate;
+        private static DateTime? Timeout;
+        private static bool buttonHeld = false;
+        private static State myState = State.Free;
+        static public GpioController controller { get; set; }
+
+        enum State
+        {
+            Working,
+            Break,
+            Free,
+            NoChange
+        }
+        private static State HasNewState()
+        {
+            if (controller.Read(Program.button1) == 1 && !buttonHeld)
+            {
+                buttonHeld = true;
+                return State.Working;
+            }
+            if (controller.Read(Program.button2) == 1 && !buttonHeld)
+            {
+                buttonHeld = true;
+                return State.Break;
+            }
+            if (controller.Read(Program.button3) == 1 && !buttonHeld)
+            {
+                buttonHeld = true;
+                return State.Free;
+            }
+            return State.NoChange;
+        }
+        private static bool AreButtonsHeld()
+        {
+            return controller.Read(Program.button1) == 1 || controller.Read(Program.button2) == 1 || controller.Read(Program.button3) == 1;
+        }
+        private static DateTime? SetTime(State s)
+        {
+            switch (s)
+            {
+                case State.Working:
+                    return DateTime.Now.Add(new TimeSpan(0, minutes:30, 0));
+                case State.Break:
+                    return DateTime.Now.Add(new TimeSpan(0, minutes:5, 0));
+                case State.Free:
+                    return null;
+            }
+            return null;
+        }
+        private static void UpdateState(State newState)
+        {
+            myState = newState;
+            FutureDate = SetTime(myState);
+            LedController.Blink((LedController.LED)newState);
+        }
+        public static void ThreadProc()
+        {
+            while (true)
+            {
+                if (buttonHeld && !AreButtonsHeld())
+                    buttonHeld = false;
+                else if (buttonHeld)
+                {
+                    LedController.BlinkAll();
+                    LedController.WaitBlink();
+                    continue;
+                }
+                State newState = HasNewState();
+                // We want to consider switching states
+                if ((newState != myState) && (newState != State.NoChange))
+                {
+                    // We aren't currently timing, continue changing state
+                    if (!Timeout.HasValue && !FutureDate.HasValue)
+                    {
+                        Console.WriteLine("We have a new state and no times");
+                        UpdateState(newState);
+                    } else if (Timeout.HasValue) 
+                    {
+                        Console.WriteLine("We have a new state but are in timeout");
+                        // We have a timeout already and have passed it
+                        if (DateTime.Now >= Timeout)
+                        {
+                            Console.WriteLine("We no longer have timeout");
+                            Timeout = null;
+                            UpdateState(newState);
+                        }
+                    } else
+                    {
+                        Console.WriteLine("Set a timeout");
+                        // We have no timeout, but we have FutureDate, lets set a timeout.
+                        Timeout = DateTime.Now.Add(new TimeSpan(0, 0, 0, 0, milliseconds:LedController.BlinkTimes*LedController.SleepTime));
+                        LedController.BlinkAll();
+                    }
+                }
+                if (FutureDate <= DateTime.Now)
+                {
+                    FutureDate = null;
+                }
+                if ((myState == State.Break || myState == State.Working) && !FutureDate.HasValue)
+                {
+                    LedController.BlinkAll();
+                }
+                if (FutureDate.HasValue)
+                {
+                    var x = FutureDate - DateTime.Now;
+                    Console.WriteLine(x.ToString());
+                }
+                LedController.SetLED((LedController.LED)myState);
+                Console.WriteLine($"My state is {myState}");
+                Thread.Sleep(SleepTime);
+            }
+        }
+    }
+}
---
+++ RPIPomodoro/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace RPIPomodoro
+{
+    class Program
+    {
+        public const int pin1 = 2;
+        public const int pin2 = 3;
+        public const int pin3 = 4;
+        public const int button1 = 5;
+        public const int button2 = 6;
+        public const int button3 = 7;
+        const int timeout = 500;
+        static void CheckForButtons(GpioController controller)
+        {
+            if (controller.Read(button1) == 1)
+                Console.WriteLine("Button1 is triggered");
+            if (controller.Read(button2) == 1)
+                Console.WriteLine("Button2 is triggered");
+            if (controller.Read(button3) == 1)
+                Console.WriteLine("Button3 is triggered");
+        }
+        static void InitInputs(GpioController controller)
+        {
+            controller.OpenPin(pin1, PinMode.Output);
+            controller.OpenPin(pin2, PinMode.Output);
+            controller.OpenPin(pin3, PinMode.Output);
+
+            controller.OpenPin(button1, PinMode.InputPullDown);
+            controller.OpenPin(button2, PinMode.InputPullDown);
+            controller.OpenPin(button3, PinMode.InputPullDown);
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting pomodoro app");
+            using (GpioController controller = new GpioController())
+            { 
+                InitInputs(controller);
+                LedController.controller = controller;
+                Thread LedProcess = new Thread(new ThreadStart(LedController.ThreadProc));
+                PomodoroController.controller = controller;
+                Thread PomodoroProcess = new Thread(new ThreadStart(PomodoroController.ThreadProc));
+                LedProcess.Start();
+                PomodoroProcess.Start();
+                LedProcess.Join();
+                PomodoroProcess.Join();
+            }
+            Console.WriteLine("Stopping pomodoro app");
+            
+        }
+        static void oldMain()
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Lets blink the LED's");
+            using (GpioController controller = new GpioController())
+            {
+                controller.OpenPin(pin1, PinMode.Output);
+                controller.OpenPin(pin2, PinMode.Output);
+                controller.OpenPin(pin3, PinMode.Output);
+
+                controller.OpenPin(button1, PinMode.InputPullDown);
+                controller.OpenPin(button2, PinMode.InputPullDown);
+                controller.OpenPin(button3, PinMode.InputPullDown);
+
+                Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs eventArgs) =>
+                {
+                    controller.Dispose();
+                };
+
+                while (true)
+                {
+                    Console.WriteLine($"Pin1 - {pin1}");
+                    CheckForButtons(controller);
+                    controller.Write(pin1, PinValue.High);
+                    controller.Write(pin2, PinValue.Low);
+                    controller.Write(pin3, PinValue.Low);
+                    Thread.Sleep(timeout);
+                    Console.WriteLine($"Pin1 - {pin2}");
+                    CheckForButtons(controller);
+                    controller.Write(pin1, PinValue.Low);
+                    controller.Write(pin2, PinValue.High);
+                    controller.Write(pin3, PinValue.Low);
+                    Thread.Sleep(timeout);
+                    Console.WriteLine($"Pin1 - {pin3}");
+                    CheckForButtons(controller);
+                    controller.Write(pin1, PinValue.Low);
+                    controller.Write(pin2, PinValue.Low);
+                    controller.Write(pin3, PinValue.High);
+                    Thread.Sleep(timeout);
+                }
+
+            }
+        }
+    }
+}
